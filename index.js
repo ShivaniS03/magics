c14f671037a8e75281edc7789158876163eedd0d
shivani <sivani.10121999@gmail.com>
2023-09-25 12:07:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        },
+        700:{
+items:2
+        }
+    }
+})
+
+
+
+$('.owl-carouseles').owlCarouseles({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        },
+        700:{
+items:2
+        }
+    }
+})
+
+
+
+
+
